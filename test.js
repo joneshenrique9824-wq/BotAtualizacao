1937f36171efbacd5453695cad466c7c7b192ffb
joneshenrique9824-wq <joneshenrique9824@gmail.com>
2026-04-27 10:49:55 -0300

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-import { Client } from "discord.js";
-
-const client = new Client({ intents: [] });
-
-client.once("ready", () => {
-  console.log("✅ BOT FUNCIONANDO");
-});
-
-client.login("MTQ5ODMxMTA0OTUxNzA3NjY0MQ.GCAGYc.w-73QVn0JuVofUMxAvWN-qp1QiR0sFetfslQ8k");
